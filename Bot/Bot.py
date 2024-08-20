698ed258455356ae7c477f56558906999adce773
Gleb Ryzhov <glebka.r@yandex.ru>
2024-08-21 00:18:39 +0300

    Сделал валидацию на ввод номера команды

--- Bot/Bot.py
+++ Bot/Bot.py
@@ -7,6 +7,15 @@ bot = telebot.TeleBot(BotAPI.botToken)
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, BotAPI.botStartMessage)
+    bot.register_next_step_handler(message, getTeamNumber)
+
+
+def getTeamNumber(message):
+    if message.text.isdigit():
+        bot.reply_to(message, "Done!")
+    else:
+        bot.reply_to(message, BotAPI.botErrorTeamNumberMessage)
+        bot.register_next_step_handler(message, getTeamNumber)
 
 
 @bot.message_handler(commands=['help'])
